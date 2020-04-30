40242ba70379daa188ab73a1f3f7d73a0fddc7b4
Jordan <jordan.a.neill@gmail.com>
2020-04-30 17:41:44 -0500

    added some js

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharcters =["!", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "", "]", "^", "_", "`", "{", "|", "}", "~"]
+
+
+
+
 // on click prompt do you want a new password
+var newPass = confirm("Do you want a new password?");
+
 // if yes bring up uppercase prompt
 // if no close window
+if (newPass) {
+    alert()
+} else alert {
+    ("Okay.");
+}
+
 
 // Ask if uppercase wanted
+var uppercase = confirm ("Do you want uppercase characters?")
+
 // if yes then array conatining uppercase letters will be used in randomizer
 // if no then move on to next prompt
+if (uppercase) {
+    
+}
 
 // ask if lowercase is wanted
 // if yes then array conataining lowercase letters will be used in randomizer
