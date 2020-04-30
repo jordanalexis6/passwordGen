72f7693c12ab65f5217334079f9713f5f9742f22
Jordan <jordan.a.neill@gmail.com>
2020-04-30 00:27:56 -0500

    html and puesdocode in js

---
+++ main.js
@@ -0,0 +1,21 @@
+// on click prompt do you want a new password
+// if yes bring up uppercase prompt
+// if no close window
+
+// Ask if uppercase wanted
+// if yes then array conatining uppercase letters will be used in randomizer
+// if no then move on to next prompt
+
+// ask if lowercase is wanted
+// if yes then array conataining lowercase letters will be used in randomizer
+// if no  then move on to next prompt
+
+// ask if numbers are wanted
+// if yes then array containing numbers will be used in randomizer
+// if no then move on to next prompt
+
+// ask if special characters are wanted
+// if yes than array containing special characters will be used in randomizer
+// if no continue to generate password
+
+// alert with new password
