71a6af83a1b4f4418dca6c5b2cadc652cf40e4bf
Jordan <jordan.a.neill@gmail.com>
2020-05-01 13:20:48 -0500

    addedJSprompts

--- main.js
+++ main.js
@@ -1,42 +1,122 @@
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharcters =["!", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "", "]", "^", "_", "`", "{", "|", "}", "~"]
-
-
-
-
-// on click prompt do you want a new password
-var newPass = confirm("Do you want a new password?");
-
-// if yes bring up uppercase prompt
-// if no close window
-if (newPass) {
-    alert()
-} else alert {
-    ("Okay.");
-}
-
-
-// Ask if uppercase wanted
-var uppercase = confirm ("Do you want uppercase characters?")
-
-// if yes then array conatining uppercase letters will be used in randomizer
-// if no then move on to next prompt
-if (uppercase) {
+var uppercase = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  var lowercase = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialCharcters = [
+    "!",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+  ];
+  var allOptions = [];
+  let password = "";
+  function generate() {
+    // on click prompt do you want a new password
+    var newPass = confirm("Do you want a new password?");
     
-}
-
-// ask if lowercase is wanted
-// if yes then array conataining lowercase letters will be used in randomizer
-// if no  then move on to next prompt
-
-// ask if numbers are wanted
-// if yes then array containing numbers will be used in randomizer
-// if no then move on to next prompt
-
-// ask if special characters are wanted
-// if yes than array containing special characters will be used in randomizer
-// if no continue to generate password
-
-// alert with new password
+      // Ask if uppercase wanted
+      var allowUppercaseChars = confirm("Do you want uppercase characters?");
+      // if yes then array conatining uppercase letters will be used in randomizer
+      
+  
+      // ask if lowercase is wanted
+      var allowLowercaseChars = confirm("Do you want lowercase characters?");
+      // if yes then array conataining lowercase letters will be used in randomizer
+     
+  
+      // ask if numbers are wanted
+      var allowNumber = confirm("Do you want numbers?");
+      // if yes then array containing numbers will be used in randomizer
+      
+  
+      // ask if special characters are wanted
+      var allowSpecialChars = confirm("Do you want special characters?");
+      // if yes than array containing special characters will be used in randomizer
+      
+  
+      //   ask how many characters they want
+      var length = prompt("How many characters do you want in your password? 8 min, 128 max.");
+        // if length if valid
+       
+  
+      // alert with new password
+     
